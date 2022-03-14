69a4c82743553de0a73b4eb6cf6db607ae01f8f5
nasiba <nasiba4647@gmail.com>
2022-03-14 12:01:00 +0500

    edit styles

--- src/Components/infoPost/infoPost.js
+++ src/Components/infoPost/infoPost.js
@@ -5,6 +5,10 @@ import ErrorPage from "../../pages/error";
 //style 
 import './infoPage.scss'
 
+//images
+import likes from '../../assets/img/like.png'
+import share from '../../assets/img/share.png'
+
 export default function InfoPage() {
 
     const [loading, setLoading] = useState(true)
@@ -32,12 +36,24 @@ export default function InfoPage() {
 
 
     return (
-        <div>
-            <h1>
-                Information
-            </h1>
-            <p>{post.body}</p>
-            <Link className="home" to="/">Go to Home </Link>
+        <div className="info">
+            <div className="info__btns">
+                <div>
+                    <img src={likes} alt="likes" />
+                    <p className="info__like">125</p>
+                </div>
+                <div>
+                    <img src={share} alt="share" />
+                    <p>70</p>
+                </div>
+            </div>
+            <div className="info__content">
+                <h1>
+                    Information
+                </h1>
+                <p className="info__text">{post.body}</p>
+                <Link className="home" to="/">Go to Home </Link>
+            </div>
 
         </div>
     )
